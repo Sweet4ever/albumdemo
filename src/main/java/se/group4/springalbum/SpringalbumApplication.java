6141aaf15768156fb1b1b3c7c19fff52cb4f0fec
Sweet4ever <oskpil@gmail.com>
2021-02-24 11:00:20 +0100

    frist commit

---
+++ src/main/java/se/group4/springalbum/SpringalbumApplication.java
@@ -0,0 +1,13 @@
+package se.group4.springalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringalbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringalbumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/group4/springalbum/SpringalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package se.group4.springalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringalbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
